17e0b4e1f2dd767180a08d77a8c8fe55e1a085e1
Jim Toth <jim@artby.city>
2023-04-14 08:19:54 -0400

    fixes contract deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,9 @@
 import { ethers } from 'hardhat'
 
 async function main () {
+  const [ owner ] = await ethers.getSigners()
   const contract = 'RelayRegistry'
-  const Contract = await ethers.getContractFactory(contract)
+  const Contract = await ethers.getContractFactory(contract, owner)
   console.log(`Deploying ${contract} contract`)
   const deployed = await Contract.deploy('0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199')
   await deployed.deployed()
