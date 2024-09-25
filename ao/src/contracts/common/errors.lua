5d3265de5fda9721bc92099bf438a6644f93edf8
Jim Toth <jim@artby.city>
2024-09-25 19:18:36 -0400

    Adds Relay Registry Verified Hardware features (#140)

--- ao/src/contracts/common/errors.lua
+++ ao/src/contracts/common/errors.lua
@@ -2,6 +2,8 @@ return {
   OnlyOwner = 'This method is only available to the Owner',
   InvalidCertificate = 'Invalid certificate',
   FingerprintRequired = 'Fingerprint required',
+  FingerprintsRequired = 'A comma-separated list of Fingerprints is required',
+  DuplicateFingerprint = 'Duplicate Fingerprint',
   UnknownFingerprint = 'Unknown Fingerprint',
   OnlyRelayOperatorCanRenounce = 'Only the Relay Operator can renounce',
   AddressIsBlocked = 'Address is blocked',
