710abe06b88afd187d1659e90440cfc9af03e979
Jim Toth <jim@artby.city>
2024-12-19 20:26:03 -0500

    Adds enforcement of ACL on operator-registry contract handles

--- ao/test/spec/contracts/relay-rewards/acl.spec.ts
+++ ao/test/spec/contracts/relay-rewards/acl.spec.ts
@@ -9,12 +9,34 @@ import {
   OWNER_ADDRESS
 } from '~/test/util/setup'
 
-describe('ACL of relay rewards', () => {
+describe('ACL enforcement of relay rewards', () => {
   let handle: AOTestHandle
 
   beforeEach(async () => {
     handle = (await createLoader('relay-rewards')).handle
   })
 
-  it('todo -> enforcing roles tests')
+  describe('Update-Configuration', () => {
+    it('Allows Admin Role')
+
+    it('Allows Update-Configuration Role')
+  })
+
+  describe('Add-Scores', () => {
+    it('Allows Admin Role')
+
+    it('Allows Add-Scores Role')
+  })
+
+  describe('Complete-Round', () => {
+    it('Allows Admin Role')
+
+    it('Allows Complete-Round Role')
+  })
+
+  describe('Cancel-Round', () => {
+    it('Allows Admin Role')
+
+    it('Allows Cancel-Round Role')
+  })
 })
