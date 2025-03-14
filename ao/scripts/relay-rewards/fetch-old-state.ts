7b5c7e3f04d0f01419e351022ffcd2da1b1b5c77
kanshi <46557+kanshi@users.noreply.github.com>
2025-03-14 12:07:35 +0100

    State migration for relay-rewards

--- ao/scripts/relay-rewards/fetch-old-state.ts
+++ ao/scripts/relay-rewards/fetch-old-state.ts
@@ -8,16 +8,17 @@ import {
   sendAosMessage
 } from '../send-aos-message'
 import { oldState } from './oldState'
+import { setMaxIdleHTTPParsers } from 'http'
 
 dotenv.config()
 
-const ethPrivateKey = process.env.ETH_PRIVATE_KEY
+// const ethPrivateKey = process.env.ETH_PRIVATE_KEY
 const processId = process.env.RELAY_REWARDS_PROCESS_ID
 const operatorProcessId = process.env.OPERATOR_REGISTRY_PROCESS_ID
 
-if (!ethPrivateKey) {
-  throw new Error('ETH_PRIVATE_KEY is not set!')
-}
+// if (!ethPrivateKey) {
+//   throw new Error('ETH_PRIVATE_KEY is not set!')
+// }
 
 if (!processId) {
   throw new Error('RELAY_REWARDS_PROCESS_ID is not set!')
@@ -27,10 +28,10 @@ if (!operatorProcessId) {
   throw new Error('OPERATOR_REGISTRY_PROCESS_ID is not set!')
 } else { logger.info(`Operator registry process ID: ${operatorProcessId}`)}
 
-const signer = new EthereumSigner(ethPrivateKey)
-logger.info(
-  `Using wallet with public key ${signer.publicKey.toString('hex')}`
-)
+// const signer = new EthereumSigner(ethPrivateKey)
+// logger.info(
+//   `Using wallet with public key ${signer.publicKey.toString('hex')}`
+// )
 
 async function fetchOldState() {
   logger.info(`Calling View-State on Relay Rewards AO Process ${processId}`)
@@ -50,7 +51,7 @@ async function fetchOldState() {
 
 async function getForFingerprint(fingerprint: string) {
   const { result } = await sendAosDryRun({
-    processId: operatorProcessId!,
+    processId: processId!,
     //signer: await createEthereumDataItemSigner(signer) as any,
     tags: [
       { name: 'Action', value: 'Get-Rewards' },
@@ -58,22 +59,52 @@ async function getForFingerprint(fingerprint: string) {
       { name: 'Fingerprint', value: fingerprint}
     ]
   })
-  console.log(result)
+  return result.Messages[0].Data
+}
+
+
+async function getForAddress(address: string) {
+  const { result } = await sendAosDryRun({
+    processId: processId!,
+    //signer: await createEthereumDataItemSigner(signer) as any,
+    tags: [
+      { name: 'Action', value: 'Get-Rewards' },
+      { name: 'Address', value: address }
+    ]
+  })
   return result.Messages[0].Data
 }
 
 async function doFlow() {
   // const oldState = await fetchOldState()
 
-  const fingerprints = Object.keys(oldState.VerifiedFingerprintsToOperatorAddresses)
-  const addresses = [ ... new Set(Object.values(oldState.VerifiedFingerprintsToOperatorAddresses))]
+  const fingerprints = Object.keys(oldState.Messages[0].Data.VerifiedFingerprintsToOperatorAddresses)
+  const addresses = [ ... new Set(Object.values(oldState.Messages[0].Data.VerifiedFingerprintsToOperatorAddresses))]
   
   logger.info(`Processing fingerprints ${fingerprints.length} addresses: ${addresses.length}`)
 
-  // fingerprints.forEach(async (fingerprint) => {
-    const rewardForFingerprint = await getForFingerprint(fingerprints[0])
-    console.log(`${fingerprints[0]}: ${rewardForFingerprint}`)
-  // })
+  console.log(`{ "PreviousRound": { "Period": 3600, "Timestamp": 1741948079386},`)
+  console.log(`  "Configuration": { "TokensPerSecond": "40509259200000000", "Multipliers": {"Family": {"Enabled": true,"Offset": 0.02,"Power": 0.7}, "Location": {"Offset": 0.001,"Enabled": true,"Divider": 20,"Power": 1.85}}, "Modifiers": {"ExitBonus": {"Share": 0.1,"Enabled": true},"Hardware": {"Enabled": true,"Share": 0.2,"UptimeInfluence": 0.35}, "Uptime": {"Enabled": true,"Share": 0.14,"Tiers": {"0": 0,"3": 1,"14": 3}}, "Network": {"Share": 0.56}}},`)
+  console.log(`  "TotalFingerprintReward": {`)
+  for(var i = 0; i < fingerprints.length; i++) {
+    const rewardForFingerprint = await getForFingerprint(fingerprints[i])
+    if (rewardForFingerprint) {
+      console.log(`    "${fingerprints[i]}": "${rewardForFingerprint}",`)
+    }
+    await new Promise(resolve => setTimeout(resolve, 100))
+  }
+  console.log(`  },`)
+
+  console.log(`  "TotalAddressReward": {`)
+  for(var i = 0; i < addresses.length; i++) {
+    const rewardForAddress = await getForAddress(addresses[i])
+    if (rewardForAddress) {
+      console.log(`    "${addresses[i]}": "${rewardForAddress}",`)
+    }
+    await new Promise(resolve => setTimeout(resolve, 100))
+  }
+  console.log(`  }`)
+  console.log(`}`)
 }
 
 doFlow().catch(e => { logger.error(e); process.exit(1); })
--- ao/test/spec/contracts/relay-rewards/view-init-state.spec.ts
+++ ao/test/spec/contracts/relay-rewards/view-init-state.spec.ts
@@ -11,8 +11,63 @@ import {
 describe('relay-rewards-view-init-state', () => {
   let handle: AOTestHandle
 
+  const specInit = { "PreviousRound": { "Period": 3600, "Timestamp": 1741948079386},
+  "Configuration": { "TokensPerSecond": "40509259200000000", "Multipliers": {"Family": {"Enabled": true,"Offset": 0.02,"Power": 0.7}, "Location": {"Offset": 0.001,"Enabled": true,"Divider": 20,"Power": 1.85}}, "Modifiers": {"ExitBonus": {"Share": 0.1,"Enabled": true},"Hardware": {"Enabled": true,"Share": 0.2,"UptimeInfluence": 0.35}, "Uptime": {"Enabled": true,"Share": 0.14,"Tiers": {"0": 0,"3": 1,"14": 3}}, "Network": {"Share": 0.56}}},
+  "TotalFingerprintReward": {
+    "9DCE2E73FC950AC0DD85A2614EF5F00F282401C6": "830055776974343241",
+    "ABEC4F5FA4895FB77F4C0A9059D8174E050B9BBA": "2297212781963434939",
+    "F52CA3CDDAEC905D2E6990B0B360F806DB59DA47": "11722828725171727047",
+    "59E36B707147CD59FBF9930D187DCDDEB4AD20A2": "388463949103352195",
+    "438089BDA922294AC19520A56F96379908777BB5": "46721406387021560357",
+    "CD20E11505272E43E09DB466FAE5C6441F8D1CBD": "11691864665962394141",
+    "A6B11EA5869B1B3739C45A14FAD556FDEE27F322": "1871735693086640027",
+    "1FB00400CD0E2C8800A119F8C3AC7F28F706AE70": "33182720496911801296",
+    "7D693A06CABA96D745196B39E637E827A93890FF": "2208730951654556152",
+    "3E44C3A83DCC7E14A2002C65863FF84631C44F17": "3644330753531429946"
+  },
+  "TotalAddressReward": {
+    "0xAD44FD9EF2F27F8D8133AD2C579EF3EEEFEB76CB": "43105548995409966667",
+    "0xF058F64D2B74C7DF161C237E81356E57328B3813": "426289041170755537361",
+    "0xAB8F493E39B9288DE834059C26428EB25C5AE325": "970102674393447307014",
+    "0x13C4A32471B1865234C135B5F0C4355DB8C88C1E": "659248036235349016311",
+    "0x1BF4D23AE938A2CEB343FA1507D77452B441C6E5": "479811718243165158381",
+    "0x7C62F3B9A9501803A4241570626C9008F08FC543": "213350898431721858763",
+    "0x2B75B8223FE26622A4540CED3A49EFDE53AE4E6C": "417990877231594478341",
+    "0xE2C5A3168B4A877FCE42BE91A9F42D1AC571CF32": "2208730951654556152",
+    "0xF411E412A80F373A67A40749DFF62D53DCBA45D9": "606092987519126062372",
+    "0x68EAD76BCAC4E0F558312EA8FB1B884091D008E3": "40071342305560394463"
+  }
+}
+
   beforeEach(async () => {
     handle = (await createLoader('relay-rewards')).handle
+  })
+
+  it('works with the reference init data', async() => {
+    const initStateResult = await handle({
+      From: OWNER_ADDRESS,
+      Tags: [
+          { name: 'Action', value: 'Init' }
+      ],
+      Data: JSON.stringify(specInit)
+    })
+    expect(initStateResult.Messages).to.have.lengthOf(1)
+    expect(initStateResult.Messages[0].Data).to.equal('OK')
+
+    const checkStateResult = await handle({
+      From: OWNER_ADDRESS,
+      Tags: [
+          { name: 'Action', value: 'View-State' }
+      ],
+      Data: JSON.stringify(specInit)
+    })
+    expect(initStateResult.Messages).to.have.lengthOf(1)
+    const state2 = checkStateResult.Messages[0].Data
+    const s = specInit
+    const s2 = JSON.parse(state2)
+    expect(s.PreviousRound.Period).to.be.equal(s2.PreviousRound.Period)
+    expect(s.PreviousRound.Timestamp).to.be.equal(s2.PreviousRound.Timestamp)
+    expect(s.Configuration.TokensPerSecond).to.be.equal(s2.Configuration.TokensPerSecond)
   })
 
   it('allows for reimport of the state during init', async () => {
