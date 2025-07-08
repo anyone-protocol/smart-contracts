10a959917637e14d5d67abbc8724119bd364ee6f
Jim Toth <jim@artby.city>
2025-07-08 06:27:05 -0400

    Update send-aos-message util script to allow for setting CU_URL

--- ao/scripts/send-aos-message.ts
+++ ao/scripts/send-aos-message.ts
@@ -1,12 +1,20 @@
 import {
   createDataItemSigner,
-  message as aoMessage,
-  result as aoResult,
-  dryrun as aoDryRun
+  connect as aoConnect
 } from '@permaweb/aoconnect'
 import { createData, Signer } from 'arbundles'
 
 import { logger } from './util/logger'
+
+export const CU_URL = process.env.CU_URL
+if (!CU_URL) {
+  throw new Error('CU_URL is not set!')
+}
+const {
+  message: aoMessage,
+  result: aoResult,
+  dryrun: aoDryRun,
+} = aoConnect({ CU_URL })
 
 export type SendAosBaseOptions = {
   processId: string
