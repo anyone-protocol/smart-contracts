2e1369a9f907886624c92878ca253b50e0ec1bf3
Jim Toth <jim@artby.city>
2025-12-02 12:46:33 -0500

    Adds --convert flag to dryrun-view-roles script to convert dumped acl state into  migrateable acl state

--- ao/scripts/dryrun-view-roles.ts
+++ ao/scripts/dryrun-view-roles.ts
@@ -2,18 +2,33 @@ import 'dotenv/config'
 import { sendAosDryRun } from './send-aos-message'
 
 const processId = process.env.PROCESS_ID || ''
-
 if (!processId) {
   throw new Error('PROCESS_ID is not set!')
 }
+
+const convertToMigratableState = process.argv.includes('--convert')
 
 async function dryrunViewState() {
   const result = await sendAosDryRun({
     processId,
     tags: [ { name: 'Action', value: 'View-Roles' } ]
   })
-  if (result.result.Messages[0]) {
-    console.log(result.result.Messages[0].Data)
+  if (result.result?.Messages && result.result.Messages[0]) {
+    if (convertToMigratableState) {
+      const parsedAcl = JSON.parse(result.result.Messages[0].Data)
+      const migratedAcl: any = { Grant: {} }
+      for (const role in parsedAcl.Roles) {
+        console.log('Role:', role)
+        for (const address in parsedAcl.Roles[role]) {
+          console.log('  Address:', address)
+          migratedAcl.Grant[address] = migratedAcl.Grant[address] || []
+          migratedAcl.Grant[address].push(role)
+        }
+      }
+      console.log(JSON.stringify(migratedAcl, null, 2))
+    } else {
+      console.log(result.result.Messages[0].Data)
+    }
   } else {
     console.error('Result error:', JSON.stringify(result.result, null, 2))
   }
